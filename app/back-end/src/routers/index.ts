f92b3c251927ab89b128b2d1b871e83b8fc4409c
Gabriela Muniz <gabrielanamu@live.com>
2023-05-24 14:47:50 -0300

    fix: package.json and back-end/routers imports

--- app/back-end/src/routers/index.ts
+++ app/back-end/src/routers/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import nameRouter from './nameRouter';
 import IngredientRouter from './ingredients.router';
 import randomRouter from './random.router'
+import AreaRouter from './area.router';
 
 const router = Router();
  router.use('/drinks', nameRouter);
@@ -10,6 +11,7 @@ const router = Router();
  router.use('/meals', IngredientRouter);
  router.use('/drinks', randomRouter);
  router.use('/meals', randomRouter);
+ router.use('/meals', AreaRouter)
 
  export default router;
 
